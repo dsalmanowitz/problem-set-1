cafbc9830e9b4a2acdcb434f5d9809a30c9e5d2e
Dennis Salmanowitz <dsalmanowitz@ucvts.org>
2018-09-18 11:13:06 -0400

    hi

--- src/ProblemSet1.java
+++ src/ProblemSet1.java
@@ -15,13 +15,33 @@ public class ProblemSet1 {
 	
 	public static void main(String[] args) {
 		System.out.println("/// EXERCISE 1 /////////////////////////////////////////////////\n");
+		System.out.println("   ##    ######  ######## ########");
+		System.out.println("  #  #   #     # #        #");
+		System.out.println(" #    #  #     # #        #");
+		System.out.println("######## ######  #        ########");
+		System.out.println("#      # #       #               #");
+		System.out.println("#      # #       #               #");
+		System.out.println("#      # #       ######## ########\n");
 		
 		System.out.println("/// EXERCISE 2 /////////////////////////////////////////////////\n");
+		System.out.println("   /\\     /\\     /\\ ");
+		System.out.println("  /  \\   /  \\   /  \\ ");
+		System.out.println(" /    \\ /    \\ /    \\ ");
+		System.out.println(" \\    / \\    / \\    / ");
+		System.out.println("  \\  /   \\  /   \\  / ");
+		System.out.println("   \\/     \\/     \\/ \n");
 		
-		System.out.println("/// EXERCISE 3 /////////////////////////////////////////////////\n");
+		System.out.print("/// EXERCISE 3 /////////////////////////////////////////////////\n\n");
+		System.out.print("    \\\\\\\\\\\n");
+		System.out.print("   +\"\"\"\"\"+\n");
+		System.out.print(" (|  0 0  |)\n");
+		System.out.print("  |   ^   |\n");
+		System.out.print("  |  '-'  |\n");
+		System.out.print("   +-----+\n\n");
 		
-		System.out.println("/// EXERCISE 4 /////////////////////////////////////////////////\n");
+		System.out.println("/// EXERCISE 4 /////////////////////////////////////////////////\n\n H E L L O\n E       A\n L       P\n L       C\n O A P C S\n");
 		
 		System.out.println("/// EXERCISE 5 /////////////////////////////////////////////////\n");
+		System.out.println("\"The answer's quite simple,\" the professor said. \"Just think outside the box!\"");
 	}
 }
